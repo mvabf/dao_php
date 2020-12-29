1e29415eb5e948c593f6ca877a199b9f4e6ae82b
mvabf <mvabf_dev@outlook.com>
2020-12-29 20:38:51 -0300

    class usuario criada

--- config.php
+++ config.php
@@ -2,7 +2,8 @@
 
 spl_autoload_register(function($className) {
     
-    $filename = $className . ".php";
+    $dirClass = 'class';
+    $filename = str_replace("\\","/", $dirClass . DIRECTORY_SEPARATOR . $className . ".php");
 
     if (file_exists($filename)) {
         require_once($filename);

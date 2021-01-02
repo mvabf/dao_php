8b1bc9fad277779b2a714e6649e85673ea410b82
mvabf <mvabf_dev@outlook.com>
2021-01-02 11:31:25 -0300

    todas os metodos de busca implementados

--- index.php
+++ index.php
@@ -2,8 +2,21 @@
 
 require_once('config.php');
 
-$root = new Usuario();
+// Carrega somente um usuário
+// $root = new Usuario();
+// $root->loadById(3);
+// echo $root;
 
-$root->loadById(3);
+// Carrega uma lista de usuários
+// $lista = Usuario::getList();
+// echo json_encode($lista);
 
-echo $root;
+// Carrega uma lista de usuário, buscando pelo login
+// $search = Usuario::search('jo');
+// echo json_encode($search);
+
+// Carrega somente um usuário, atenticando por login e senha
+$usuario = new Usuario();
+$usuario->login("root","#%$#%");
+echo $usuario;
+

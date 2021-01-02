65e521d306ce56b34b876d03cde1e15d28c7a856
mvabf <mvabf_dev@outlook.com>
2021-01-02 12:03:12 -0300

    iniciado insert com procedure no mysql

--- class/Usuario.php
+++ class/Usuario.php
@@ -49,12 +49,8 @@ class Usuario {
         ));
 
         if (isset($results[0])) {
-            $row = $results[0];
 
-            $this->setIdusuario($row['idusuario']);
-            $this->setDeslogin($row['deslogin']);
-            $this->setDessenha($row['dessenha']);
-            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+            $this->setData($results[0]);
         }
     }
     // Estático pois não há uso do this
@@ -86,18 +82,38 @@ class Usuario {
         ));
 
         if (isset($results[0])) {
-            $row = $results[0];
 
-            $this->setIdusuario($row['idusuario']);
-            $this->setDeslogin($row['deslogin']);
-            $this->setDessenha($row['dessenha']);
-            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+            $this->setData($results[0]);
+            
         } else {
 
             throw new Exception("Login e/ou Senha inválidos!", 1);
 
         }
 
+    }
+
+    public function setData($data) {
+
+            $this->setIdusuario($data['idusuario']);
+            $this->setDeslogin($data['deslogin']);
+            $this->setDessenha($data['dessenha']);
+            $this->setDtcadastro(new DateTime($data['dtcadastro']));
+    }
+
+    public function insert() {
+
+        $sql = new Sql();
+
+        $results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+            ":LOGIN"=>$this->getDeslogin(),
+            ":PASSWORD"=>$this->getDessenha()
+        ));
+
+        if (isset($results)) {
+
+            $this->setData($results[0]);
+        }
     }
 
     public function __toString() {
